616d5873e7f41185c4f54d163eedec3862dd2cbb
Jarle Syvertsen <wolfinabarrel@protonmail.com>
2022-03-20 16:04:16 +0100

    Pushing to git

---
+++ server.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const express = require('express');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+// const cookieSessionModule = require('cookie-session');
+const cookieSession = require('cookie-session');
+
+
+require('dotenv').config();
+
+const helmet = require('helmet');
+
+const PORT = 8000;
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2
+}
+
+const app = express();
+
+app.use(helmet());
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [ config.COOKIE_KEY_1, config.COOKIE_KEY_2 ]
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET
+}
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+    console.log('Google profile', profile);
+    done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+// Save session to cookie.
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+// Read session from cookie.
+passport.deserializeUser((id, done) => {
+    done(null, id);
+});
+
+function checkLoggedIn(req, res, next) 
+{
+    console.log('Current user is:', req.user)
+    const isLoggedIn = req.isAuthenticated() && req.user;
+    if(!isLoggedIn) {
+        return res.status(401).json({
+            error: "You must be logged in to access these resources."
+        })
+    }
+    next();
+}
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['email'],
+}));
+
+app.get('/auth/google/callback', 
+    passport.authenticate('google', {
+        failureRedirect: '/failure',
+        successRedirect: '/',
+        session: true,
+
+    }), 
+    (req, res) => {
+        console.log('Google callback reached');
+    }
+);
+
+app.get('/auth/logout', (req, res) => {
+    req.logout();
+    return res.redirect('/');
+});
+
+app.get('/failure', (req,res) => {
+    return res.send('Failed to log in.');
+})
+
+app.get('/', (req,res) => {
+    return res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.get('/secret', checkLoggedIn, (req,res) => {
+   return res.send('Your personal secret value is 2314!');
+})
+
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
+    console.log(`listening on PORT ${PORT}`);
+})
